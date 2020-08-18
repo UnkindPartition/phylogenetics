5d0ce7dc4bffae1f855635c86d1f7f2346cb0e93
Roman Cheplyaka <roma@ro-che.info>
2020-08-18 21:20:45 +0300

    docs

--- src/Phylogenetics/Types.hs
+++ src/Phylogenetics/Types.hs
@@ -4,9 +4,11 @@ import qualified Data.IntMap.Strict as IntMap
 import qualified Data.Vector.Unboxed as VU
 import Data.Word
 
+-- | A node in a phylogenetic tree
 newtype NodeId = NodeId Int
   deriving newtype Show
 
+-- | The length of a branch in a phylogenetic tree
 newtype BranchLength = BranchLength Double
   deriving newtype (Eq, Ord, Num, Real, Fractional, RealFrac, Show)
 
@@ -28,9 +30,11 @@ getNodeId = \case
 newtype BranchLengths = BranchLengths (IntMap.IntMap BranchLength)
   deriving newtype Show
 
+-- | Extract the branch length leading to a given node
 getBranchLength :: BranchLengths -> NodeId -> BranchLength
 getBranchLength (BranchLengths bl) (NodeId node_id) = bl IntMap.! node_id
 
+-- | A set of observed characters per site
 data Observations = Observations
   { numOfSites :: !Int
   , characters :: !(IntMap.IntMap (VU.Vector Word8))
@@ -40,11 +44,12 @@ data Observations = Observations
 -- | The transition matrix stored in the row-major order
 data EvolutionModel = EvolutionModel (VU.Vector Double)
 
+-- | Calculate the transition probability from one nucleotide to another
 transitionProbability
   :: EvolutionModel
   -> BranchLength
-  -> Word8 -- from
-  -> Word8 -- to
+  -> Word8 -- ^ from nucleotide
+  -> Word8 -- ^ to nucleotide
   -> Double
 transitionProbability (EvolutionModel mx) (BranchLength len) from to =
   exp $ len * VU.unsafeIndex mx (fromIntegral $ from * 4 + to)

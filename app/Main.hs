81144bc37f0ef441ac34e4dbb61b96d29ffc3b62
Roman Cheplyaka <roma@ro-che.info>
2020-10-18 10:34:33 +0300

    Write info.csv

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ import Data.Random
 import Data.Random.Source.PureMT
 import Data.IORef
 import Data.Word
-import Text.Printf (printf)
+import Text.Printf
 import System.IO
 import System.Clock
 import Phylogenetics.Types
@@ -72,50 +72,58 @@ trace
   -> Int
     -- ^ number of leaves in a tree
   -> IO ()
-trace num_steps seed num_sites num_leaves = do
-  hSetBuffering stdout LineBuffering
-  rnd_src <- newIORef (pureMT seed)
+trace num_steps seed num_sites num_leaves =
+  withFile "trace.csv" WriteMode $ \trace_h ->
+  withFile "info.csv" WriteMode $ \info_h -> do
+    hSetBuffering trace_h LineBuffering
+    hSetBuffering info_h LineBuffering
+    rnd_src <- newIORef (pureMT seed)
 
-  (prob, true_bls) <- flip runRVar rnd_src $ do
-    rate_mx <- rateMatrix bd
-    topo <- topology bd
-    bls <- branchLengths bd topo
-    obs <- realisticObservations bd rate_mx topo bls
-    return (Problem rate_mx obs topo, bls)
+    (prob, true_bls) <- flip runRVar rnd_src $ do
+      rate_mx <- rateMatrix bd
+      topo <- topology bd
+      bls <- branchLengths bd topo
+      obs <- realisticObservations bd rate_mx topo bls
+      return (Problem rate_mx obs topo, bls)
 
-  let init_bls = 0.1 <$ true_bls
+    let
+      init_bls = 0.1 <$ true_bls
+      true_ll = logLikelihood prob true_bls
 
-  printf "method,step,rate,ll,mse,time\n"
+    hPrintf info_h "var,value\n"
+    hPrintf info_h "true_ll,%.6f\n" true_ll
 
-  forM_ methods $ \Method{..} -> do
-    printf "%s,0,NA,%.6f,%.6g,0\n"
-      methodName
-      (logLikelihood prob init_bls)
-      (calculateMSE init_bls true_bls)
-    flip evalStateT (TraceState (methodInit prob init_bls)) $
-      forM_ [1 .. num_steps] $ \istep ->
-      runMaybeT $ do
-        prev_state <- get
-        t0 <- liftIO $ getTime ProcessCPUTime
-        let step_result = methodStep prob (trace_method_state prev_state)
-        case step_result of
-          Nothing -> pure ()
-          Just (bls, actual_learning_rate, ll, _) -> do
-            liftIO . evaluate . rnf $ bls
-            liftIO . evaluate . rnf $ actual_learning_rate
-            liftIO . evaluate . rnf $ ll
-        t1 <- liftIO $ getTime ProcessCPUTime
-        let
-          time :: Double
-          time = fromIntegral (toNanoSecs (diffTimeSpec t1 t0)) / 1e9
-        case methodStep prob (trace_method_state prev_state) of
-          Just (bls, actual_learning_rate, ll, st) -> do
-            put $! TraceState st
-            liftIO $ printf "%s,%d,%.4g,%.6f,%.6g,%.3g\n" methodName istep actual_learning_rate ll (calculateMSE bls true_bls) time
-            --liftIO $ print bls
-          Nothing -> mzero
+    hPrintf trace_h "method,step,rate,ll,mse,time\n"
 
-  return ()
+    forM_ methods $ \Method{..} -> do
+      hPrintf trace_h "%s,0,NA,%.6f,%.6g,0\n"
+        methodName
+        (logLikelihood prob init_bls)
+        (calculateMSE init_bls true_bls)
+      flip evalStateT (TraceState (methodInit prob init_bls)) $
+        forM_ [1 .. num_steps] $ \istep ->
+        runMaybeT $ do
+          prev_state <- get
+          t0 <- liftIO $ getTime ProcessCPUTime
+          let step_result = methodStep prob (trace_method_state prev_state)
+          case step_result of
+            Nothing -> pure ()
+            Just (bls, actual_learning_rate, ll, _) -> do
+              liftIO . evaluate . rnf $ bls
+              liftIO . evaluate . rnf $ actual_learning_rate
+              liftIO . evaluate . rnf $ ll
+          t1 <- liftIO $ getTime ProcessCPUTime
+          let
+            time :: Double
+            time = fromIntegral (toNanoSecs (diffTimeSpec t1 t0)) / 1e9
+          case methodStep prob (trace_method_state prev_state) of
+            Just (bls, actual_learning_rate, ll, st) -> do
+              put $! TraceState st
+              liftIO $ hPrintf trace_h "%s,%d,%.4g,%.6f,%.6g,%.3g\n" methodName istep actual_learning_rate ll (calculateMSE bls true_bls) time
+              --liftIO $ print bls
+            Nothing -> mzero
+
+    return ()
   where
     bd = dnaBaseDistributions
       { numberOfSitesDistribution = pure num_sites
